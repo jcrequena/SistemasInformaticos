802f28c799e282a07d030e917a9877f13dbf1e5a
Juan Carlos <jrequenav@iesmiralcamp.org>
2016-08-10 13:43:20 +0200

    Update Ejercicio1.c

--- src-UD1/Ejercicio1.c
+++ src-UD1/Ejercicio1.c
@@ -2,6 +2,8 @@
 // El archivo de cabecera de la biblioteca estándar de C limits.h 
 // se utiliza para establecer mediante constantes ciertas propiedades de las variables de tipo entero, 
 // propiedades como por ejemplo la definición de su rango.
+// http://www.tutorialspoint.com/c_standard_library/limits_h.htm
+// http://tigcc.ticalc.org/doc/limits.html
 #include <limits.h>
 
 int main()
