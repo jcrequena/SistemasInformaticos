19c2dd6afb9273105f521cbb82733ef2aa27c48a
Juan Carlos <jrequenav@iesmiralcamp.org>
2017-03-17 19:17:51 +0100

    Update read_file_csv.sh

--- UD6/Fase1-Proyecto/read_file_csv.sh
+++ UD6/Fase1-Proyecto/read_file_csv.sh
@@ -1,20 +1,21 @@
-File_Users_csv="C:\usuarios.csv"
+fntCrearUsuarios() {   
 
-if not exist %File_Users_csv% (goto error) else (goto adduser)
+# read c1,c2 lee las columnas que le indicamos, c1 y c2 pueden tener otro valor, ejemplo: campo1,campo2, c1,c2, etc..
+#Para acceder a las columnas se hace: ${columna}
+	while IFS=, read c1,c2 
+	do
+   		echo "Columna 1 "${c1}
+   		echo "Columna 2 "${c2}
+	done < $File_Users_csv #aquí se pone el fichero csv que se quiere recorrer en el bucle
+	return
+} 
 
-:adduser    
-REM La opción /F permite que el for recorra un fichero
-REM tokens son las distintas columnas del fichero csv, en este caso, hay 6 campos (columnas)
-REM delims=; indica que las columnas se separan por el carácter ;
-REM skip=1 indica que la primera línea del fichero no se toma en cuenta. Normalmente, la primera fial
-REM son los nombres de los campos.
-REM %%a primer campo del fichero (a), el segundo será el b, el tercero el c, etc...
-REM in (%File_Users_csv%) fichero que se va a recorrer en el bucle
-REM 
-FOR /F "tokens=1,2,3,4,5,6 delims=; skip=1" %%a in (%File_Users_csv%) do (
-  echo Procesando el usuario con información %%a %%b %%c %%d %%e %%f
-)
-goto Etiqueta
-:error
-echo "Error de acceso al fichero"
 
+File_Users_csv=/home/root/usuarios.csv
+
+if [ -f $File_Users_csv ]; then
+  fnt_CrearUsuarios
+else
+  
+  echo "Error"
+fi
