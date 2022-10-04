0aa17715d063ec5a6774ef51e2e85141530f47e7
Juan Carlos <jcrequena.fp@gmail.com>
2022-10-04 13:59:56 +0200

    Update Ejercicio2.c

--- UT1/src/Ejercicio2.c
+++ UT1/src/Ejercicio2.c
@@ -6,11 +6,10 @@
 // http://tigcc.ticalc.org/doc/limits.html
 #include <limits.h>
 
-int main()
+void main()
 
 {
    char a=127,b=127,c;
    c=a+b;
    printf("a=%d b=%d c=a+b=%d",a,b,c);
-   return 0;
 }
