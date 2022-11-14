5099c80a48be5c6623098696595dc1d327ff8a8a
Juan Carlos <jcrequena.fp@gmail.com>
2022-11-14 18:43:05 +0100

    Create setPassphrasePolicy.ps1

---
+++ UT3/PowerShell/setPassphrasePolicy.ps1
@@ -0,0 +1,17 @@
+#Establecer política de contraseñas (ejemplo: Longitud mínima 12, complejidad de la contraseña sí) para un Dominio (brs.ciber)
+#En este ejemplo se crea una directiva habilitada con las siguientes propiedades:
+#Requiere una contraseña compleja
+#Al menos 12 caracteres de longitud
+#Caduca la contraseña a los 10 días (MaxPasswordAge D.H:M:S.F )
+#Recuerda las 5 últimas contraseñas (PasswordHistoryCount 5)
+#Se configura para que se bloquee si se falla al intento 3.
+#Duración del bloqueo 60 minutos
+#Ventana de bloqueo (tiempo en minutos antes de que se restablezca el contador de intentos fallidos de contraseña )
+#fuente: https://learn.microsoft.com/en-us/powershell/module/activedirectory/set-addefaultdomainpasswordpolicy?view=windowsserver2019-ps
+
+
+Set-ADDefaultDomainPasswordPolicy -Identity brs.ciber -MinPasswordLength 12 -ComplexityEnabled $True 
+                                  -PasswordHistoryCount 5 -MaxPasswordAge 10.00:00:00 -LockoutThreshold 3 
+                                  -LockoutDuration 0.0:30:0 -LockoutObservationWindow 0.0:30:0 -ReversibleEncryptionEnabled $False 
+#Para consultar la configuración de los parámetros establecidos
+Get-ADDefaultDomainPasswordPolicy
