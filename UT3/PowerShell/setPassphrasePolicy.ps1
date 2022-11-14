1e778032a04a7b7120f3dbf39be798c5ce1ea455
Juan Carlos <jcrequena.fp@gmail.com>
2022-11-14 18:57:36 +0100

    Update setPassphrasePolicy.ps1

--- UT3/PowerShell/setPassphrasePolicy.ps1
+++ UT3/PowerShell/setPassphrasePolicy.ps1
@@ -1,17 +1,21 @@
-#Establecer política de contraseñas (ejemplo: Longitud mínima 12, complejidad de la contraseña sí) para un Dominio (brs.ciber)
-#En este ejemplo se crea una directiva habilitada con las siguientes propiedades:
-#Requiere una contraseña compleja
-#Al menos 12 caracteres de longitud
-#Caduca la contraseña a los 10 días (MaxPasswordAge D.H:M:S.F )
-#Recuerda las 5 últimas contraseñas (PasswordHistoryCount 5)
+#Establecer política de contraseñas con comandos NET
+#Eemplo: Longitud mínima 8, tiempo de cambio de contraseña entre 2 y 60 días, bloqueo de cuenta con 3 intentos erróneos.
+
+#Bloqueo de cuentas 
 #Se configura para que se bloquee si se falla al intento 3.
+net accounts /lockoutthreshold:3
 #Duración del bloqueo 60 minutos
+net accounts /lockoutduration:60
 #Ventana de bloqueo (tiempo en minutos antes de que se restablezca el contador de intentos fallidos de contraseña )
-#fuente: https://learn.microsoft.com/en-us/powershell/module/activedirectory/set-addefaultdomainpasswordpolicy?view=windowsserver2019-ps
+net accounts /lockoutwindow:60
 
+#Establecer los dias máximos para cambiar el password, ejempo 60 días.
+net accounts /maxpwage:60
+#Establecer los dias mínimos para cambiar el password, ejempo 2 días.
+net accounts /minpwage:2
+#Establecer la longitud mínima del password, ejemplo: 8
+net accounts /minpwlen:8
 
-Set-ADDefaultDomainPasswordPolicy -Identity brs.ciber -MinPasswordLength 12 -ComplexityEnabled $True 
-                                  -PasswordHistoryCount 5 -MaxPasswordAge 10.00:00:00 -LockoutThreshold 3 
-                                  -LockoutDuration 0.0:30:0 -LockoutObservationWindow 0.0:30:0 -ReversibleEncryptionEnabled $False 
 #Para consultar la configuración de los parámetros establecidos
-Get-ADDefaultDomainPasswordPolicy
+net accounts 
+
