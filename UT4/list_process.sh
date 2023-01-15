8fe699267be61034fc556ef46ce487459b652bb4
Juan Carlos <jcrequena.fp@gmail.com>
2023-01-15 13:34:58 +0100

    Update list_process.sh

--- UT4/list_process.sh
+++ UT4/list_process.sh
@@ -3,12 +3,12 @@
 # Provides:       list_process
 # Required-Start: 
 # Required-Stop: 
-# Default-Start:  5
-# Default-Stop:   
+# Default-Start:  3 4 5
+# Default-Stop:   1 2
 # Short-Description: List State Process
 ### END INIT INFO
 
 echo `date` >> /var/log/process-list.dat
-echo "---------------------------------" >> /var/log/estado-procesos.dat
-ps -aux >> /var/log/process-list.dat
 echo "---------------------------------" >> /var/log/process-list.dat
+ps -aux >> /var/log/process-list.dat
+
