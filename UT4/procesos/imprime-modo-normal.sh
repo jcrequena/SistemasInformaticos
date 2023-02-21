500f8b83d481920fbf5027af247d246a6bd6fdf9
Juan Carlos <jcrequena.fp@gmail.com>
2023-02-21 17:37:21 +0100

    Create imprime-modo-normal.sh

---
+++ UT4/procesos/imprime-modo-normal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+echo `date +'%A %d de %B del %Y. Hora: %H:%M'`
+echo " Creaciones Caminàs"
+echo " -----------------"
+echo " Comienza la impresión normal...."
+#Sale del bucle cuando sean las 08:00 de la mañana.
+while [ "$hora" != "08:00" ];do
+    hora=$(date +"%R")
+done
+exit 0
