bbeda312e478f8d7ebe35ffd361eae1babd51f5d
Juan Carlos <jcrequena.fp@gmail.com>
2023-02-22 08:30:25 +0100

    Update exec-scripts-impresora.sh

--- UT4/procesos/exec-scripts-impresora.sh
+++ UT4/procesos/exec-scripts-impresora.sh
@@ -1,5 +1,3 @@
-  GNU nano 2.9.3             exec-scripts-impresora.sh                          
-
 #!/bin/bash
 #El script se ejcutará como sudo: sudo exec-scripts-impresora.sh
 
@@ -8,9 +6,10 @@ echo `date +'%A %d de %B del %Y. Hora: %H:%M'`
 echo "Creaciones Caminàs"
 echo "-----------------"
 echo " Se ejecutan los scripts de impresión...."
+echo "------------------------------------------"
 echo " Ejecución de impresión en modo normal...."
 ./imprime-modo-normal.sh &
+echo ""
+echo "--------------------------------------"
 echo " Ejecución de impresión en modo 3d...."
 ./imprime-en-3d.sh &
-
-exit 0
