77d3576f94151784c9972d3e9927075377bacb76
Juan Carlos <jcrequena.fp@gmail.com>
2023-02-22 07:53:45 +0100

    Update imprime-en-3d.sh

--- UT4/procesos/imprime-en-3d.sh
+++ UT4/procesos/imprime-en-3d.sh
@@ -4,7 +4,7 @@ echo `date +'%A %d de %B del %Y. Hora: %H:%M'`
 echo " Creaciones Caminàs"
 echo " -----------------"
 echo " Comienza la impresión en 3D...."
-#SAle del bucle cuando sean las 08:00 de la mañana.
+#Sale del bucle cuando sean las 08:00 de la mañana.
 while [ "$hora" != "08:00" ];do
     hora=$(date +"%R")
 done
