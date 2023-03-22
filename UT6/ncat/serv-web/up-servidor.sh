5fb78659bbadb5df1451252892a9eea413a68a74
Juan Carlos <jcrequena.fp@gmail.com>
2023-03-22 15:54:39 +0100

    Update up-servidor.sh

--- UT6/ncat/serv-web/up-servidor.sh
+++ UT6/ncat/serv-web/up-servidor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Usamos el puerto 1200 para poner a la escucha el servidor
-while true; do ncat -v -l -p 12000 -c ./servidor-web.sh; done
+#Usamos el puerto 12000 para poner a la escucha el servidor
+while true; do ncat -v -l -p 12000 -c ./serv-web.sh; done
