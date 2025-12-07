2b54e820ecfc751992b77f94dc6cf25e88369e45
Juan Carlos <jcrequena.fp@gmail.com>
2025-12-07 18:22:49 +0100

    Update user creation to remove expiration flags
    Removed the flags for AccountNeverExpires and PasswordNeverExpires when creating a new local user.

--- UT3/PowerShell/usuariosMasivos.ps1
+++ UT3/PowerShell/usuariosMasivos.ps1
@@ -4,7 +4,7 @@
 $file_users=Import-Csv -Path usuarios.csv 
 foreach ($user in $file_users) { 
   $clave=ConvertTo-SecureString $user.password -AsPlainText -Force
-  New-LocalUser $user.cuenta -Password $clave -Description $user.descripcion -AccountNeverExpires -PasswordNeverExpires
+  New-LocalUser $user.cuenta -Password $clave -Description $user.descripcion
   #Añadimos la cuenta de usuario en el grupo de Usuarios del sistema
   Add-LocalGroupMember -Group usuarios -Member $user.cuenta
   Write-Host "Usuario $user.cuenta creado"
