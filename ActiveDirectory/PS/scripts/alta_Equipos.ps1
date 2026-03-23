a40cd3dfe2089e331223554437afee6b43e54ca7
Juan Carlos <jcrequena.fp@gmail.com>
2026-03-23 18:26:21 +0100

    Add script to create AD computers from CSV
    This script creates computers in Active Directory based on a CSV file input.

---
+++ ActiveDirectory/PS/scripts/alta_Equipos.ps1
@@ -0,0 +1,27 @@
+#Referencia: https://technet.microsoft.com/es-es/library/hh852258(v=wps.630).aspx
+#Para este caso de ejemplo, usamos el csv que contiene lo siguiente
+#Computer:Path
+#W11-CLI001:OU=Equipos-DepInf,OU=Dep-Informatica
+#W11-CLI002:OU=Equipos-DepInf,OU=Dep-Informatica
+#Es decir, queremos crear los equipos, en la OU Equipos-DepInf que esta dentro de la OU Dep-Informatica
+#Por lo tanto para crear el equipo en esa ruta sobre el dominio si.mylocal quedaría así:
+#OU=Equipos-DepInf,OU=Dep-Informatica,DC=si,DC=mylocal
+
+#Ponemos el Domain Component para el dominio en cuestión, que para este caso es smr.local
+$domain="dc=si,dc=mylocal"
+
+#
+#Creación de los grupos a partir de un fichero csv
+#
+#Lee el fichero grupos.csv. El carácter delimitador de columna es :
+$equiposCsv=Read-Host "Introduce el fichero csv de Equipos:"
+$fichero= import-csv -Path $equiposCsv -delimiter ":"
+
+foreach($line in $fichero)
+{
+	New-ADComputer -Enabled:$true -Name:$line.Computer -Path:$line.Path -SamAccountName:$line.Computer
+}
+
+write-Host ""
+write-Host "Se han creado los equipos en el dominio $domain" -Fore green
+write-Host "" 
